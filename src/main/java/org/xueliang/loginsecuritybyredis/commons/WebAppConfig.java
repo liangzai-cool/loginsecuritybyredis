8d669ee2e3e80f83a9e33965bf0150f13789d485
liangzai_cool <liangzai_cool@yeah.net>
2016-11-02 13:38:18 +0800

    config welcome page

--- src/main/java/org/xueliang/loginsecuritybyredis/commons/WebAppConfig.java
+++ src/main/java/org/xueliang/loginsecuritybyredis/commons/WebAppConfig.java
@@ -8,26 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "org.xueliang.loginsecuritybyredis")
-public class WebAppConfig  {
+@PropertySource({"classpath:loginsecuritybyredis.properties"})
+public class WebAppConfig extends WebMvcConfigurerAdapter {
 	
+	/**
+	 * 内容协商
+	 * @return
+	 */
 	@Bean
 	public ContentNegotiationManagerFactoryBean contentNegotiationManagerFactoryBean() {
 	    ContentNegotiationManagerFactoryBean contentNegotiationManagerFactoryBean = new ContentNegotiationManagerFactoryBean();
 	    contentNegotiationManagerFactoryBean.setFavorParameter(true);
 	    contentNegotiationManagerFactoryBean.setIgnoreAcceptHeader(true);
-	    contentNegotiationManagerFactoryBean.setDefaultContentType(MediaType.APPLICATION_JSON);
+	    contentNegotiationManagerFactoryBean.setDefaultContentType(MediaType.APPLICATION_JSON_UTF8);
 	    Properties mediaTypesProperties = new Properties();
-	    mediaTypesProperties.setProperty("json", MediaType.APPLICATION_JSON_VALUE);
+	    mediaTypesProperties.setProperty("json", MediaType.APPLICATION_JSON_UTF8_VALUE);
 	    contentNegotiationManagerFactoryBean.setMediaTypes(mediaTypesProperties);
 		return contentNegotiationManagerFactoryBean;
 	}
@@ -40,10 +48,13 @@ public class WebAppConfig  {
 		return contentNegotiatingViewResolver;
 	}
 	
+	/**
+	 * 采用UTF-8编码，防止中文乱码
+	 * @return
+	 */
 	@Bean
 	public StringHttpMessageConverter stringHttpMessageConverter() {
-		StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
-		return stringHttpMessageConverter;
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
 	}
 	
 	@Bean
@@ -53,15 +64,12 @@ public class WebAppConfig  {
 		return requestMappingHandlerAdapter;
 	}
 	
-//	@Override
-//	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-//		configurer.favorPathExtension(false).
-//        favorParameter(true).
-//        parameterName("mediaType").
-//        ignoreAcceptHeader(true).
-//        useJaf(false).
-//        defaultContentType(MediaType.APPLICATION_JSON).
-//        mediaType("xml", MediaType.APPLICATION_XML).
-//        mediaType("json", MediaType.APPLICATION_JSON);
-//	}
+	/**
+	 * 设置欢迎页
+	 * 相当于web.xml中的 welcome-file-list > welcome-file
+	 */
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/index.html");
+	}
 }
