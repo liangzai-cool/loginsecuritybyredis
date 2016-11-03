243e3c7bb8b32defdf9c286dc94dcce9c74d799b
liangzai_cool <liangzai_cool@yeah.net>
2016-11-03 11:29:27 +0800

    remove dead code

--- src/main/java/org/xueliang/loginsecuritybyredis/web/controller/api/AuthApi.java
+++ src/main/java/org/xueliang/loginsecuritybyredis/web/controller/api/AuthApi.java
@@ -1,12 +1,9 @@
 package org.xueliang.loginsecuritybyredis.web.controller.api;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +37,7 @@ public class AuthApi {
 	private Jedis jedis;
 	
 	@PostConstruct
-	public void init() throws FileNotFoundException, IOException {
+	public void init() {
 		for (int i = 0; i < 3; i++) {
 			String username = "username" + 0;
 			String password = "password" + 0;
@@ -50,7 +47,7 @@ public class AuthApi {
 	}
 	
 	@RequestMapping(value = {"login"})
-	public String login(HttpSession session, @RequestParam("username") String username, @RequestParam("password") String password) {
+	public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
 		JSONResponse jsonResponse = new JSONResponse();
 		String key = username;
 		String countString = jedis.get(key);
@@ -93,18 +90,16 @@ public class AuthApi {
 			jsonResponse.addMsg("success", "恭喜，登录成功！");
 			return;
 		}
+		jsonResponse.setCode(1);
 		if (count >= MAX_TRY_COUNT) {
 			long pttlSeconds = jedis.pttl(key) / 1000;
 			long hours = pttlSeconds / 3600;
 			long sencondsRemain = pttlSeconds - hours * 3600;
 			long minutes = sencondsRemain / 60;
 			long seconds = sencondsRemain - minutes * 60;
-			jsonResponse.setCode(1);
 			jsonResponse.addError("login_disabled", "登录超过" + MAX_TRY_COUNT + "次，请" + hours + "小时" + minutes + "分" + seconds + "秒后再试！");
 			return;
 		}
-		jsonResponse.setCode(1);
 		jsonResponse.addError("username_or_password_is_wrong", "密码错误，您还有 " + (MAX_TRY_COUNT - count) + " 次机会！");
-		return;
 	}
 }
